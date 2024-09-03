229795ba6195274413d48a8a42417c76d4ad8ed5
cerengunaydin <crnn.gnydnn@gmail.com>
2024-09-03 23:07:45 +0300

    social-media-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login"
+
+
+function App() {
+  return <Profile/>
+}
+
+export default App;
